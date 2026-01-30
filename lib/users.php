8571aeade36b8e204b718997b0cbf3633d298bf3
sirKonstantinos <186953456+sirKonstantinos@users.noreply.github.com>
2026-01-30 17:12:23 +0200

    Update users.php

--- lib/users.php
+++ lib/users.php
@@ -6,7 +6,7 @@ require_once "lib/game.php";
 function handle_user($method, $b,$input) {
 	if($method=='GET') {
 		show_user($b);
-	} else if($method=='PUT') { 
+	} else if($method=='PUT' || $method=='POST') { 
         set_user($b,$input);
     }
 }
